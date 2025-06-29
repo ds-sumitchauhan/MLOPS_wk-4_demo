cbc58453f269cba6ce6d3c5ee3ebafc948d65fa2
sumit <sumitchauhan1986@gmail.com>
2025-06-29 19:39:06 +0000

    Initial pipeline with training and tests

---
+++ src/model_trainer.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def train_and_evaluate(df):
+    X = df.drop(columns=["species"])
+    y = df["species"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
+    acc = accuracy_score(y_test, model.predict(X_test))
+    return model, acc
